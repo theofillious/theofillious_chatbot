c4d51b3812c6cf7323e8e5134016e5eaffdfc90a
theofillious <thapelonthorela08@gmail.com>
2025-01-20 13:59:15 +0200

    Previous normal version without auto complete

--- src/app/Actionprovider.js
+++ src/app/Actionprovider.js
@@ -48,37 +48,32 @@ class ActionProvider {
            }
         );
         return chatCompletion.choices[0].message.content;
- };  
-    handleSuggestion = (suggestion) => {
-    this.callGenAI(suggestion);
-};
+ }; 
+    timer = ms => new Promise(res => setTimeout(res, ms));
 
-timer = ms => new Promise(res => setTimeout(res, ms));
-
-generateResponseMessage = async (userMessage) => {
-    const responseFromGPT = await this.callGenAI(userMessage);
-    let message;
-    let numberNoLines = responseFromGPT.split('\n') .length;
-    for (let i = 0; i < numberNoLines; i++) {
-       const msg = responseFromGPT.split('\n')[i];
-       if(msg.length){
-        message = this.createChatBotMessage(msg);
-        this.updateChatBotMessage(message);
-       }
-       await this.timer(1000);
+    generateResponseMessage = async (userMessage) => {
+        const responseFromGPT = await this.callGenAI(userMessage);
+        let message;
+        let numberNoLines = responseFromGPT.split('\n').length;
+        for (let i = 0; i < numberNoLines; i++) {
+            const msg = responseFromGPT.split('\n')[i];
+            if (msg.length) {
+                message = this.createChatBotMessage(msg);
+                this.updateChatBotMessage(message);
+            }
+            await this.timer(1000);
+        }
     }
-}
 
-respond = (message) => {
-    this.generateResponseMessage(message)
-}
+    respond = (message) => {
+        this.generateResponseMessage(message)
+    }
 
-updateChatBotMessage = (message) => {
-    this.setState(prevState => ({
-        ...prevState, messages: [...prevState.messages, message]
-    }))
-}
-
+    updateChatBotMessage = (message) => {
+        this.setState(prevState => ({
+            ...prevState, messages: [...prevState.messages, message]
+        }))
+    };
 }
 
 export default ActionProvider;
--- src/app/components/ChatComponent.js
+++ src/app/components/ChatComponent.js
@@ -4,38 +4,23 @@ import 'react-chatbot-kit/build/main.css';
 import config from "../config";
 import ActionProvider from "../Actionprovider";
 import MessageParser from "../MessageParser";
-import AutoComplete from './AutoComplete';
+ 
 
-const suggestions = [
-  'How to change a tire?',
-  'What is the best oil for my car?',
-  'How to check engine oil level?',
-  'What does the check engine light mean?',
-  'How to jump-start a car?',
-  'How to replace windshield wipers?',
-  'What is the recommended tire pressure?',
-  'How to change car battery?',
-  'How to fix a flat tire?',
-  'What is the maintenance schedule for my car?'
-];
+ 
  
 
 const ChatComponent = () => {
-    const actionProvider = new ActionProvider();
-    const handleSelectSuggestion = (suggestion) => {
-        actionProvider.handleSuggestion(suggestion);
-    };
-
+ 
     return (
-        <div className="chatbot-container">
+        <div className="chatbot-icon">
             
             <Chatbot
              config = {config}
              actionProvider = {ActionProvider}
              messageParser = {MessageParser}
-             messageHistory = {[]}   
+                 
             />
-            <AutoComplete suggestions = {suggestions} onSelect = {handleSelectSuggestion} />
+             
         </div>
     )
     
